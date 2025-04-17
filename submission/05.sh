9c50b4412ee267f7accea9deaeb12349a5e10be3
yhzlsm <yhzlsm@gmail.com>
2025-04-17 10:57:01 -0300

    feat: add CSV script with 150 blocks

--- submission/05.sh
+++ submission/05.sh
@@ -1,2 +1,28 @@
 # Create a CSV script that would lock funds until one hundred and fifty blocks had passed
-# publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+
+op_push_data_2bytes_hex="02"
+
+time_hex=$(printf '%08x' 1495584032)
+time_hex_little_endian=$(echo "$time_hex" | sed 's/../& /g' | awk '{for(i=NF;i>0;i--) printf $i}')
+
+blocks=$(printf 150)
+hex=$(printf '%08x\n' $blocks | sed 's/^\(00\)*//');
+hexfirst=$(echo $hex | cut -c1)
+[[ 0x$hexfirst -gt 0x7 ]] && hex="00"$hex
+lehex=$(echo $hex | tac -rs .. | echo "$(tr -d '\n')");
+
+op_checksequenceverify="b2"
+op_drop="75"
+op_dup="76"
+op_hash160="a9"
+op_push_data_20bytes_hex="14"
+
+publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+publicKey_hash=$(echo -n "$publicKey" | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -ripemd160 | awk '{print $2}')
+
+op_equalverify="88"
+op_checksig="ac"
+
+script_hex="${op_push_data_2bytes_hex}${lehex}${op_checksequenceverify}${op_drop}${op_dup}${op_hash160}${op_push_data_20bytes_hex}${publicKey_hash}${op_equalverify}${op_checksig}"
+
+echo "$script_hex"
